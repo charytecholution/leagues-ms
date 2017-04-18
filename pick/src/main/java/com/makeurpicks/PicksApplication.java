bf7fdda565bfb65c89786a18eea3a2c942204bdd
Hariharan Anantharaman <hariharan.anantharaman@techolution.com>
2017-04-18 14:27:11 +0530

    Changes to Pick Service for MYP67. Also includes changes to persist pick in MySQL

--- pick/src/main/java/com/makeurpicks/PicksApplication.java
+++ pick/src/main/java/com/makeurpicks/PicksApplication.java
@@ -1,23 +1,47 @@
 package com.makeurpicks;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
 @EnableResourceServer
 public class PicksApplication {
+	
+	
+	@LoadBalanced
+	@Bean(name={"loadBalancedRestTemplate"})
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	@Autowired
+	AuthorizationCodeResourceDetails oAuth2ProtectedResourceDetails;
+	@Autowired
+	OAuth2ClientContext oAuth2ClientContext;
+	@LoadBalanced
+	@Bean
+	public OAuth2RestOperations securerestTemplate() {
+		return new OAuth2RestTemplate(oAuth2ProtectedResourceDetails, oAuth2ClientContext);
+	}
 
     public static void main(String[] args) {
         SpringApplication.run(PicksApplication.class, args);
