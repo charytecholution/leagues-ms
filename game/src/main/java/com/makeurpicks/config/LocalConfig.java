ab1d75c2a8510b6c4cb58c604db24e2860e82505
Timothy Delesio <tdelesio@Timothys-MacBook-Pro.local>
2017-04-18 09:48:25 -0400

    fixed c3p0 error in game, added flag for docker:push, fixed some errors in the application ymls

--- game/src/main/java/com/makeurpicks/config/LocalConfig.java
+++ game/src/main/java/com/makeurpicks/config/LocalConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
+//import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 @Configuration
 @Profile("local")
